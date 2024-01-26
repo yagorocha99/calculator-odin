d228776112afe2f48598235a85ad2ffc71c0811d
Yago <yagovr.rocha@gmail.com>
2024-01-26 08:26:51 -0300

    Simple functions created

---
+++ src/js/script.js
@@ -0,0 +1,37 @@
+const firstNumber = '';
+const secondNumber = '';
+const operator = '';
+
+
+function add (a, b){
+    return a + b;
+}
+
+function subtract (a, b){
+    return a - b;
+};
+
+function divide (a, b){
+    return a / b;
+};
+
+function multiply (a, b){
+    return a * b;
+};
+
+function operate(operator, a, b){
+    switch(operator){
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '/':
+            return divide(a, b);
+        case '*':
+            return multiply(a, b);
+        default:
+            return 'Invalid operator';
+    }
+}
+
+const result = operate(operator, firstNumber, secondNumber);
