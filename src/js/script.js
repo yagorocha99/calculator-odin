2db34044b1be5f17d7a57bb288d9fb2764dd92e2
Yago <yagovr.rocha@gmail.com>
2024-01-27 22:35:32 -0300

    Reafactored script

--- src/js/script.js
+++ src/js/script.js
@@ -1,75 +1,80 @@
-    let firstNumber = 0;
-    let secondNumber = '';
-    let operator = '';
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
 
-    function storeValue(value) {
-        value = String(value);
-    
-        if (value === 'C') {
-            firstNumber = 0;
-            secondNumber = '';
-            operator = '';
-            document.querySelector('#result').innerHTML = '';
-        } else if (value === '=') {
+
+function updateDisplay() {
+    document.querySelector('#result').innerHTML = `${firstNumber} ${operator} ${secondNumber || ''}`;
+}
+function storeValue(value) {
+    if (value === 'C'){
+        firstNumber = '';
+        secondNumber = '';
+        operator = '';
+        document.querySelector('#result').innerHTML = '';
+        return;
+    }
+    if (value === '='){
+        calculateResult();
+        return;
+    }
+    if (value === '+' || value === '-' || value === '*' || value === '/'){
+        if (operator !== '') {
             calculateResult();
-            return;
-        } else {
-            if (value === '+' || value === '-' || value === '*' || value === '/') {
-                if (operator !== '') {
-                    calculateResult();
-                }
-                operator = value;
-                document.querySelector('#result').innerHTML = `${firstNumber} ${operator} ${secondNumber || ''}`;
-            } else {
-                (operator === '') ? firstNumber = parseFloat(`${firstNumber}${value}`) : secondNumber = parseFloat(`${secondNumber}${value}`);
-                document.querySelector('#result').innerHTML = `${firstNumber} ${operator} ${secondNumber || ''}`;
-            }
         }
+        operator = value;
+        updateDisplay();
+        return;
     }
-
-    function calculateResult() {
-        let resultSpan = document.querySelector('#result');
-        let result;
-    
-        if (operator !== '' && !isNaN(secondNumber)) {
-            result = operate(operator, firstNumber, secondNumber);
-            resultSpan.innerHTML = result;
-            firstNumber = result;
-            secondNumber = '';
-            operator = '';
-        } else {
-            resultSpan.innerHTML = 'Invalid input';
-        }
+    else {
+        operator === '' ? (firstNumber = parseFloat(`${firstNumber}${value}`)) : (secondNumber = parseFloat(`${secondNumber}${value}`));
+        updateDisplay();
+        return;
     }
+}
 
-    function add (a, b){
-        return a + b;
+function calculateResult() {
+    let resultSpan = document.querySelector('#result');
+    let result;
+
+    if (operator !== '' && !isNaN(secondNumber)) {
+        result = operate(operator, firstNumber, secondNumber);
+        resultSpan.innerHTML = result;
+        firstNumber = result;
+        secondNumber = '';
+        operator = '';
+    } else {
+        resultSpan.innerHTML = 'Invalid input';
     }
+}
 
-    function subtract (a, b){
-        return a - b;
-    };
+function add (a, b){
+    return a + b;
+}
 
-    function divide (a, b){
-        return a / b;
-    };
+function subtract (a, b){
+    return a - b;
+};
 
-    function multiply (a, b){
-        return a * b;
-    };
+function divide (a, b){
+    return a / b;
+};
 
-    function operate(operator, a, b){
-        switch(operator){
-            case '+':
-                return add(a, b);
-            case '-':
-                return subtract(a, b);
-            case '/':
-                return divide(a, b);
-            case '*':
-                return multiply(a, b);
-            default:
-                return 'Invalid operator';
-        }
+function multiply (a, b){
+    return a * b;
+};
+
+function operate(operator, a, b){
+    switch(operator){
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '/':
+            return divide(a, b);
+        case '*':
+            return multiply(a, b);
+        default:
+            return 'Invalid operator';
     }
-
+}
