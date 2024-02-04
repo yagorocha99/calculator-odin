76c273306ea5090216f52c5130d44f590a97bffd
Yago <yagovr.rocha@gmail.com>
2024-02-04 15:43:05 -0300

    Finished calculator (or no)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
     extend: {
       fontFamily: {
         'calculator': ['Calculator']
+      },
+      colors:{
+        'operator' : '#8693AB',
+        'op-hover' : '#434D60',
+        'number' : '#AAB9CF',
+        'nb-hover' : '#496083',
+        'background' : '#212227',
+        'display' : '#BDD4E7',
+        'calculator' : '#637074',
+        
       }
     },
   },
